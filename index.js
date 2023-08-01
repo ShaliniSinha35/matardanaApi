6da95d05175cb1eaa964556dc396cbefe0ce9c54
ShaliniSinha35 <shalinisinha3560@gmail.com>
2023-08-01 16:56:43 +0530

    commit

--- index.js
+++ index.js
@@ -3,23 +3,25 @@ const cors = require("cors");
 const app = express();
 const path=require("path")
 app.use(cors());
-// app.use("/images", express.static(path.join(__dirname, "images")));
-// const data = require("./data.json");
-// const category = require("./category");
 const logo = require("./logo.json");
 
 const port = process.env.PORT || 5000;
+// app.use("/images", express.static(path.join(__dirname, "images")));
+// const data = require("./data.json");
+// const category = require("./category");
+
 
 // app.use(express.static('public'));
-app.use('/images', express.static('images'));
+// app.use('/images', express.static('images'));
 
 
 // Serve the images
 
 
-app.get("/", (req, res) => {
-  res.json(logo);
+app.get('/data', (req, res) => {
+  return res.status(200).json(logo);
 });
+
 
 
 
