cc4bd51a5b8b2d98e035157f385ec57b5b5cc937
ShaliniSinha35 <shalinisinha3560@gmail.com>
2023-08-01 16:17:33 +0530

    commit

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const path=require("path")
-
 app.use(cors());
-const data = require("./data.json");
-const category = require("./category");
+// const data = require("./data.json");
+// const category = require("./category");
 const logo = require("./logo.json");
 
 const port = process.env.PORT || 5000;
@@ -13,24 +12,23 @@ const port = process.env.PORT || 5000;
 // app.use(express.static('public'));
 // app.use('/images', express.static('images'));
 
-app.get("/", (req, res) => {
-  res.send(data);
-});
+
 // Serve the images
 app.use("/images", express.static(path.join(__dirname, "images")));
 
-app.get("/logo", (req, res) => {
+app.get("/", (req, res) => {
   return res.status(200).json(logo);
 });
 
 
 
-app.get("/category", (req, res) => {
-  res.send(category);
-});
+// app.get("/category", (req, res) => {
+//   res.send(category);
+// });
 // app.use('/images',express.static('images'))
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
+
 // https://demo-api-3fxf.vercel.app/
