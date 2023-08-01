de6d037a66cff40e1381aa6871e58f5f0bbe76a0
ShaliniSinha35 <shalinisinha3560@gmail.com>
2023-08-01 12:41:38 +0530

    logo.json added

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors")
+const app = express();
+
+
+app.use(cors())
+const data=require("./data.json")
+const category=require("./category")
+const logo=require("./logo.json")
+
+const port = process.env.PORT || 8000
+
+
+
+app.get("/", (req, res) => {
+  res.send(data);
+});
+app.get("/logo", (req, res) => {
+  res.send(logo);
+});
+app.get("/category", (req, res) => {
+  res.send(category);
+});
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
+// https://demo-api-3fxf.vercel.app/
