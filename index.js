3df864303842f36685be21ca055d6a216a64424c
ShaliniSinha35 <shalinisinha3560@gmail.com>
2023-08-02 15:29:23 +0530

    commit

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ app.get('/product', (req, res) => {
   const newData = products.map(item => {
     return {
       ...item,
-      img: `${req.protocol}://${req.get('host')}${item.img}` //  full image URL
+      img: `${req.protocol}://${req.get('host')}${item.images[0]}` //  full image URL
     };
   });
   return res.status(200).json(newData);
