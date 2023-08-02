cf587fc1a44bad82d12683f5553a2b735357d1e7
ShaliniSinha35 <shalinisinha3560@gmail.com>
2023-08-02 10:20:15 +0530

    commit

--- index.js
+++ index.js
@@ -4,20 +4,34 @@ const app = express();
 const path=require("path")
 app.use(cors());
 const logo = require("./logo.json");
+const banner=require("./banner.json")
 
 const port = process.env.PORT || 5000;
 // app.use("/images", express.static(path.resolve(__dirname, "/images")));
 // const data = require("./data.json");
 // const category = require("./category");
+
+
 app.use(express.static('public'));
 app.use('/public/images/logo', express.static(path.resolve(__dirname,'public/images/logo')));
 
 
-// Serve the images
-
-
+// Serve the logo
 app.get('/', (req, res) => {
   const newData = logo.map(item => {
+    return {
+      ...item,
+      img: `${req.protocol}://${req.get('host')}${item.img}` //  full image URL
+    };
+  });
+  return res.status(200).json(newData);
+
+});
+
+// banner
+app.use('/public/images/banner', express.static(path.resolve(__dirname,'public/images/banner')));
+app.get('/banner', (req, res) => {
+  const newData = banner.map(item => {
     return {
       ...item,
       img: `${req.protocol}://${req.get('host')}${item.img}` //  full image URL
@@ -37,4 +51,4 @@ app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
 
-// https://demo-api-3fxf.vercel.app/
+
