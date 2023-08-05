13842a9f9ceb1f78fd2db7551cce22e734ced59e
ShaliniSinha35 <shalinisinha3560@gmail.com>
2023-08-05 12:00:00 +0530

    commit

--- index.js
+++ index.js
@@ -13,10 +13,28 @@ const port = process.env.PORT || 5000;
 
 
 app.use(express.static('public'));
-app.use('/public/images/logo', express.static(path.resolve(__dirname,'public/images/logo')));
+
+
+// products
+app.use('/public/images/product', express.static(path.resolve(__dirname,'public/images/product')));
+app.get('/', (req, res) => {
+  const newData = products.map(item => {
+    return {
+      ...item,
+      img: `${req.protocol}://${req.get('host')}${item.images[0]}` //  full image URL
+
+    };
+  });
+  return res.status(200).json(newData);
+
+
+});
+
+
 
 
 // Serve the logo
+app.use('/public/images/logo', express.static(path.resolve(__dirname,'public/images/logo')));
 app.get('/logo', (req, res) => {
   const newData = logo.map(item => {
     return {
@@ -54,26 +72,7 @@ app.get('/category', (req, res) => {
 
 });
 
-// products
-app.use('/public/images/product', express.static(path.resolve(__dirname,'public/images/product')));
-app.get('/', (req, res) => {
-  // const newData = products.map(item => {
-  //   return {
-  //     ...item,
-  //     img: `${req.protocol}://${req.get('host')}${item.images[0]}` //  full image URL
 
-  //   };
-  // });
-  // return res.status(200).json(newData);
-  const newData = category.map(item => {
-    return {
-      ...item,
-      img: `${req.protocol}://${req.get('host')}${item.img}` //  full image URL
-    };
-  });
-  return res.status(200).json(newData);
-
-});
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
